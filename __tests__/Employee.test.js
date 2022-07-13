ae68e57c6e6b16b6ace02663adb0c768b0aa72cf
kodapassey <kodapassey@charter.net>
2022-07-13 15:24:43 -0700

    styling for generated page complete

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,3 +2,13 @@ const Engineer = require('../lib/Engineer');
 const Intern = require('../lib/Intern');
 const Manager = require('../lib/Manager');
 const Employee = require('../lib/Employee');
+
+
+test('creates a manager employee object', () => {
+    const manager = new Manager('Koda', 2, 'email', 'Manager');
+
+    expect(manager.name).toBe('Koda');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toBe('email');
+    expect(manager.role).toBe('Manager');
+});
